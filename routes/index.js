18f74cb3c56a61ba1824f1a1a7a6ea9582e701d5
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-08-03 16:20:14 +0200

    working on routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/register", function (req, res, next) {
+  res.send("load register form");
+});
+
+router.post("/new", function (req, res, next) {
+  res.send("send register form, create user");
 });
 
 module.exports = router;
